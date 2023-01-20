727712af1f2a81f7c148225572a1336042b3ac6b
pestyld <styl4891@gmail.com>
2023-01-20 18:30:25 +0000

    Change hmeq to home_equity

--- Using Python in SAS Studio in SAS Viya/Getting Started with Python in SAS Studio/Getting Started with PROC Python.sas
+++ Using Python in SAS Studio in SAS Viya/Getting Started with Python in SAS Studio/Getting Started with PROC Python.sas
@@ -11,12 +11,13 @@ pd.set_option('display.max_columns', None)
 ## Access Data
 df_raw = pd.read_csv(r'https://support.sas.com/documentation/onlinedoc/viya/exampledatasets/home_equity.csv')
 
+
 ## Prepare Data
 df = (df_raw
       .fillna(df_raw[df_raw.select_dtypes(include = np.number).columns.to_list()].mean())         ## Fill all numeric missing values with the mean
       .fillna(df_raw[df_raw.select_dtypes(include = object).columns.to_list()].mode().iloc[0])    ## Fill all character missing values with the mode
       .assign(DIFF = lambda _df: _df.MORTDUE - _df.VALUE,                                         ## Difference between mortgage due and value
-              BAD_CAT = lambda _df: _df.BAD.map({1:'Default', 0:'Repaid'})                        ## Map values of 1 and 0 with the values Default and Repaid        
+              LOAN_STATUS = lambda _df: _df.BAD.map({1:'Default', 0:'Repaid'})                    ## Map values of 1 and 0 with the values Default and Repaid        
        )
       .rename(columns=lambda colName:colName.lower().replace("_",""))                             ## Lowercase column names and remove underscores
 )
@@ -28,10 +29,11 @@ print(df.isna().sum())
 
 
 ## Write the DataFrame as a SAS data set in the WORK library
-SAS.df2sd(df, 'work.hmeq_compute_sas')
+SAS.df2sd(df, 'work.home_equity_compute_sas')
 
 endsubmit;
 quit;
+
 
 * Connect to the CAS Server *;
 cas conn;
@@ -39,10 +41,10 @@ cas conn;
 * Drop and load data to the CAS server *;
 proc casutil;
 	* Drop the global scope CAS table if it exists *;
-	droptable casdata='hmeq_cas_sas' incaslib="casuser" quiet;
+	droptable casdata='home_equity_cas_sas' incaslib="casuser" quiet;
 
 	* Send the SAS data set to the CAS server and promote the table *;
-	load data=work.hmeq_compute_sas casout="hmeq_cas_sas" outcaslib="casuser" promote;
+	load data=work.home_equity_compute_sas casout="home_equity_cas_sas" outcaslib="casuser" promote;
 
 	* View in-memory CAS tables *;
 	list tables;
--- Using Python in SAS Studio in SAS Viya/Getting Started with Python in SAS Studio/Getting Started with the Python Editor.py
+++ Using Python in SAS Studio in SAS Viya/Getting Started with Python in SAS Studio/Getting Started with the Python Editor.py
@@ -7,12 +7,13 @@ pd.set_option('display.max_columns', None)
 ## Access Data
 df_raw = pd.read_csv(r'https://support.sas.com/documentation/onlinedoc/viya/exampledatasets/home_equity.csv')
 
+
 ## Prepare Data
 df = (df_raw
       .fillna(df_raw[df_raw.select_dtypes(include = np.number).columns.to_list()].mean())         ## Fill all numeric missing values with the mean
       .fillna(df_raw[df_raw.select_dtypes(include = object).columns.to_list()].mode().iloc[0])    ## Fill all character missing values with the mode
       .assign(DIFF = lambda _df: _df.MORTDUE - _df.VALUE,                                         ## Difference between mortgage due and value
-              BAD_CAT = lambda _df: _df.BAD.map({1:'Default', 0:'Repaid'})                        ## Map values of 1 and 0 with the values Default and Repaid        
+              LOAN_STATUS = lambda _df: _df.BAD.map({1:'Default', 0:'Repaid'})                        ## Map values of 1 and 0 with the values Default and Repaid        
        )
       .rename(columns=lambda colName:colName.lower().replace("_",""))                             ## Lowercase column names and remove underscores
 )
@@ -28,11 +29,11 @@ print(df.isna().sum())
 ##
 
 # Load the DataFrame to the compute server as a SAS data set
-SAS.df2sd(df, 'work.hmeq_compute_py')
+SAS.df2sd(df, 'work.home_equity_compute_py')
 
 
-## Create a string with SAS code to connect to CAS server, delete the global table if it exists, and then load data to CAS
-## NOTE: You can also use the Python SWAT package to accomplish the same tasks below.
+## Create a string with SAS code to connect to the CAS server, delete the global table if it exists, and then load data to CAS
+## NOTE: You can also use the Python SWAT package to accomplish the same tasks below using all Python.
 
 prepareLoadingToCAS = '''
 * Connect to the CAS Server *;
@@ -41,10 +42,10 @@ cas conn;
 * Drop and load data to the CAS server *;
 proc casutil;
 	* Drop the global scope CAS table if it exists *;
-	droptable casdata='hmeq_cas_py' incaslib="casuser" quiet;
+	droptable casdata='home_equity_cas_py' incaslib="casuser" quiet;
 
 	* Send the SAS data set to the CAS server and promote the table *;
-	load data=work.hmeq_compute_py casout="hmeq_cas_py" outcaslib="casuser" promote;
+	load data=work.home_equity_compute_py casout="home_equity_cas_py" outcaslib="casuser" promote;
 
 	* View in-memory CAS tables *;
 	list tables;
